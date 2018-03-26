ac23bbd5cefe179c2c8abb680d983c5b14f70753
teddy.k <teddy.k@kakaocorp.com>
2018-03-26 11:17:16 +0900

    play_sound

--- play_sound/music.py
+++ play_sound/music.py
@@ -1,12 +1,22 @@
-import pygame
+from pygame import mixer
 import time
 
-pygame.mixer.init()
-bang=pygame.mixer.Sound('sample.mp3')
-
-bang.play()
+mixer.init()
+mixer.music.load('BLE_con.mp3')
+mixer.music.play()
 time.sleep(5.0)
 
+mixer.music.load('BLE_uncon.mp3')
+mixer.music.play()
+time.sleep(5.0)
+
+mixer.music.load('CAM_back.mp3')
+mixer.music.play()
+time.sleep(5.0)
+
+mixer.music.load('CAM_self.mp3')
+mixer.music.play()
+time.sleep(5.0)
 #for i in range (3):
 #    bang.play()
 #    print(i)
