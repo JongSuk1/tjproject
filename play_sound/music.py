27fe4cc0b6326a384c54ed09344f48034747cb4d
teddy.k <teddy.k@kakaocorp.com>
2018-03-26 15:25:33 +0900

    make stete check sound ftn

--- play_sound/music.py
+++ play_sound/music.py
@@ -2,18 +2,37 @@ from pygame import mixer
 import time
 
 mixer.init()
-mixer.music.load('BLE_con.mp3')
-mixer.music.play()
-time.sleep(5.0)
 
-mixer.music.load('BLE_uncon.mp3')
-mixer.music.play()
-time.sleep(5.0)
+def play_music(sound):
+    mixer.music.load(sound)
+    mixer.music.play()
+    time.sleep(5.0)
 
-mixer.music.load('CAM_back.mp3')
-mixer.music.play()
-time.sleep(5.0)
+def BLE_check(a):
+    if a == 0:
+        play_music('BLE_uncon.mp3')
+        
+    elif a == 1:
+        play_music('BLE_con.mp3')
+        
+    else:
+        print("Wrong input")
+        
 
-mixer.music.load('CAM_self.mp3')
-mixer.music.play()
-time.sleep(5.0)
+def CAM_check(a):
+    if a == 0:
+        play_music('CAM_back.mp3')
+
+    elif a == 1:
+        play_music('CAM_self.mp3')
+        
+    else:
+        print("Wrong input")   
+    
+def soundtest():
+    play_music('BLE_con.mp3')
+    play_music('BLE_uncon.mp3')
+    play_music('CAM_back.mp3')
+    play_music('CAM_self.mp3')
+
+# soundtest()
