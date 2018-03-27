29da284d198f9c7095adc6e85fa11562491a0907
jaylen.rhee <jaylen.rhee@kakaocorp.com>
2018-03-27 11:23:48 +0900

    capturing photos on pi and sending into phone via BTopex

---
+++ picamera/README.md
@@ -0,0 +1,8 @@
+v0.1 180327
+
+written by Jaylen
+
+This is test code proceding following tasks :
+1. capturing 'PHOTOCOUNT photos on picamera every 'delay sec.
+2. send it into phone with 'address via Bluetooth OPEX protocol
+3. phone user must press 'accept' button for every requests.
---
+++ picamera/send_img_BTopex.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+import picamera
+import time
+
+address = "1C:AF:05:90:F1:6C@12"
+camera = picamera.PiCamera()
+PHOTOCOUNT = 5
+photo_index = 0
+time_delay = 1
+
+#BT auto pairing
+
+#photo shoot
+while photo_index < PHOTOCOUNT :
+	camera.capture("image%d.jpg"%photo_index, resize = (320,240))
+	photo_index = photo_index + 1
+	time.sleep(time_delay)
+	print("%d th image captured"%photo_index)
+
+print("capture done")
+
+
+for i in range(1,PHOTOCOUNT):
+	filename = "image{}.jpg".format(i)
+	path_to_file = os.path.join("/home/pi/Jaylen",filename)
+	cmd = ["ussp-push", address, path_to_file, filename]
+	subprocess.call(" ".join(cmd), shell=True)
+
+print("image sent")
+
+
+
+
