5eb43ecf25e539996b3033f68285f7fe4ad42c33
teddy.k <teddy.k@kakaocorp.com>
2018-03-28 15:34:02 +0900

    take picture periodically

--- USB_camera/take_picture.py
+++ USB_camera/take_picture.py
@@ -1,30 +1,45 @@
 # Take video from camera and show it
 import numpy as np  
-import cv2  
-  
+import cv2
+import os
+import datetime
+
+def store_img(folder, img_name, frame):
+    if not os.path.isdir(folder):
+       os.mkdir(folder)
+    # make sound need
+    cv2.imwrite(folder+'/'+img_name, frame)
+
 capture = cv2.VideoCapture(0)  
 print 'image width %d' % capture.get(3)  
 print 'image height %d' % capture.get(4)  
   
 capture.set(3, 640)  
 capture.set(4, 480)  
-  
-img_num = 0  
-  
+
+period = 0
+
 while(1):  
     ret,frame = capture.read()  
-    cv2.imshow( 'webcam', frame )
+    cv2.imshow('webcam', frame )
+    
+    if period%60 == 0:
+        img_name = datetime.datetime.now().strftime('%y%m%d-%H%M%S%f')+'.jpg'
+        folder_name = datetime.datetime.now().strftime('%y%m%d')
+        store_img(folder_name, img_name, frame)
     
     key = cv2.waitKey(1)&0xFF
 
+    # auto record - store_img(folder_name,img_name,frame)
+
     if  key == ord('c'):
-        print("capture")
-        img_name = "image"+str(img_num)+".jpg"
-        cv2.imwrite(img_name,frame)
-        img_num = img_num+1
-        
+        img_name = datetime.datetime.now().strftime('%y%m%d-%H%M%S%f')+'.jpg'
+        store_img("myimage", img_name, frame)
+
     elif key == ord('q'):
         break;  
+    
+    period = period+1
 
 capture.release()  
 cv2.destroyAllWindows()  
