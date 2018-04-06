bf8d3a1dcab88d6dc0d89b4b894e0c793221cc19
jaylen.rhee <jaylen.rhee@kakaocorp.com>
2018-04-06 11:36:42 +0900

    get_baddr() added to rfcomm_server

--- app_to_pi/rfcomm_server.py
+++ app_to_pi/rfcomm_server.py
@@ -2,15 +2,24 @@ import bluetooth
 import threading
 import serial
 import sys
+import subprocess
+import re
 sys.path.insert(0, '/home/pi/tjproject/msgQueue')
 sys.path.insert(0, '/home/pi/tjproject/constants')
 import msgQueue
 from constants import *
 
+def get_baddr():
+    subprocess.call('hciconfig > hciconfig.txt', shell=True)
+    hciInfo = open('hciconfig.txt','r').read()
+    regex = re.compile("..:..:..:..:..:..")
+    baddr = regex.search(hciInfo).group()
+    return baddr
+
 class btThread(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.address = "B8:27:EB:E6:9F:B8"
+        self.address = get_baddr()
         self.port = 1
         self.Connected = False
         self.serverSock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -36,7 +45,7 @@ class btThread(threading.Thread):
         self.serverSock.listen(1)
         clientSock, clientInfo = self.serverSock.accept()
         self.Connected = True
-        msgQueue.putMsg("BTconnected")
+        msgQueue.putMsg(BT_ON)
         rdata = None # 'N' for None and is default
         while rdata != BT_OFF :# BT_OFF for disconnecting bluetooth
             try:
